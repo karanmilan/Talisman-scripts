21096d11e94c93de21e0df3624ebbf72f84e2d71
Karan Jain <karanj@thoughtworks.com>
2017-06-02 13:25:18 +0530

    Addidng another comment to make the instalation simplified

--- install-talisman-existing-repos.sh
+++ install-talisman-existing-repos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #This script is to install Talisman to existing git directories and it requires talisman to be installed as a git hook throughtout the machine and not just a single repo. You can verify by checking if \
 #the install-talisman.sh is run and there's a pre-push hook in ~/.git-templates/hooks
+#This might not be a generic script and might be designed for a specific system.
 echo "Please provide a command line arguement to the script to specify the absolute path of the repositiories, else the script will go to the home directory and install it there"
 if [ -z "$1" ]
 then
